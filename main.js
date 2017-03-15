21ff1b1fdbb1daa5ed4247bfc6c03ecc2e8b3628
Harry Coulson <coulson.mail@me.com>
2017-03-15 14:11:50 -0700

    Removing reference code.

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-/*
-<div class="container">
-  <div class="media">
-    <div class="media-left">
-      <a href="#">
-        <img class="media-object" src="brakes.jpg" alt="brakes">
-      </a>
-    </div>
-  <div class="media-body">
-    <h4 class="media-heading">Brakes</h4>
-    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-      irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-      deserunt mollit anim id est laborum.</p>
-      <span id="brakesPrice" class="price">$250.00 each</span>
-      <div class="btn-group" role="group" aria-label="...">
-        <button id= "brakesButton" type="button" class="btn btn-default">Add to cart</button>
-      </div>
-    </div>
-  </div>
-  */
-
   var items = [
     {
       mediaHeading: 'Brakes',
